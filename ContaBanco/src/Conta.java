7bfa4fac9018b86c4e87af7c805125a5caeea8d8
Rafael Escarlate <rescarlate@sinaf.com.br>
2025-03-19 10:06:50 -0300

    desenvolvimento de programa para simular uma conta bancaria conforme exercicio proposto DIO

---
+++ ContaBanco/src/Conta.java
@@ -0,0 +1,6 @@
+public class Conta {
+public int Numero;
+public String Agencia;
+public String NomeCliente;
+public double Saldo;
+}
--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner 50 = new Scanner(System.in);
+
+        Conta conta = new Conta();
+
+        System.out.println("Por favor, digite o número da conta: ");
+
+        conta.Numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da agência: ");
+
+        scanner.nextLine();
+
+        conta.Agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu nome: ");
+
+        conta.NomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo inicial da conta: ");
+
+        conta.Saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + conta.NomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + conta.Agencia + ", conta " + conta.Numero + " e seu saldo R$" + conta.Saldo + " já está disponível para saque." );
+
+        scanner.close();
     }
 }
